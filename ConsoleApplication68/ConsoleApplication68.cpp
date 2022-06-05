3dce9df8f91766d3fae1ae4f671e9d395e607f9c
Андрей Кузнецов <and.kyz.mir@gmail.com>
2022-06-05 12:34:27 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication68/ConsoleApplication68.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    string s = "/ob1/ob3/ob4";
+    string delimiter = "/";
+    vector<string> path;
+
+    size_t pos = 0;
+    string token;
+    while ((pos = s.find(delimiter)) != string::npos) {
+        token = s.substr(0, pos);
+        path.push_back(token);
+        s.erase(0, pos + delimiter.length());
+    }
+    path.push_back(s);
+
+    for (int i = 0; i < path.size(); i++)
+    {
+        cout << path[i];
+    }
+
+}
